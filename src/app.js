b4c607837dacdede6a8552f4a20f9a031fc5c55c
Oskarnuz <oscar.nunez07@gmail.com>
2023-05-27 07:54:54 -0500

    Add middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-
 const express = require('express');
 const connect = require('./config/database');
 const configExpress = require('./config/express');
 const routes = require('./routes');
+const { formData } = require("./middlewares/formData");
+
 
 
 const app = express();
@@ -13,5 +14,9 @@ configExpress(app);
 
 routes(app);
 
+// Middleware
+app.post("/test-formdata", formData, (req, res) => {
+  res.status(200).json({ ...req.body });
+});
 
 module.exports = app;
